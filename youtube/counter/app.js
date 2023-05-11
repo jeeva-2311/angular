b307484a6a88f665997998ba95b6e059515e06de
Jeeva R <75130463+jeeva-2311@users.noreply.github.com>
2023-05-11 12:28:47 +0530

    Add files via upload

---
+++ youtube/counter/app.js
@@ -0,0 +1,22 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click",(e) => {
+        if(e.currentTarget.classList.contains("decrease"))
+        count --;
+        else if(e.currentTarget.classList.contains("increase"))
+        count++;
+        else 
+        count = 0;
+
+        if(count > 0)
+        value.style.color = "green";
+        if(count < 0)
+        value.style.color = "red";
+        if(count == 0)
+        value.style.color = "black";
+        value.textContent = count;
+    });
+});
